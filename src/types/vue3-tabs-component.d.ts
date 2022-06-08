8ce323312e92818b57428b09185d0de6a0171ea8
artemaminov <artemaminov@gmail.com>
2022-06-08 17:31:24 +0300

    fix TS declaration  compile

--- src/types/vue3-tabs-component.d.ts
+++ src/types/vue3-tabs-component.d.ts
@@ -1,6 +1,1 @@
-import { DefineComponent } from 'Vue';
-
-declare module 'vue3-tabs-component' {
-  const Tabs: DefineComponent<any>;
-  const Tab: DefineComponent<any>;
-};
+declare module 'vue3-tabs-component';
